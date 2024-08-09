6356a572ac90302c8ea38b3ded52943df9b06a51
gaukhar06 <gaukhar06.00@mail.ru>
2024-08-09 17:05:52 +0500

    solve codewars tasks

--- index.js
+++ index.js
@@ -137,3 +137,141 @@ function countBy(x, n) {
 
 }
 
+function spEng(sen) {
+    return sen.toLowerCase().includes('english')
+}
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs((sonYearsOld * 2) - dadYearsOld)
+}
+
+
+function DNAtoRNA(dna) {
+    return dna.split('').map(a => a.replace('T', 'U')).join('')
+
+}
+
+function removeEveryOther(arr) {
+    return arr.filter((a, i) => i % 2 == 0)
+
+}
+
+
+function mouthSize(animal) {
+    return (animal.toLowerCase() === 'alligator') ? "small" : "wide"
+}
+mouthSize('alligator')
+
+
+function combat(health, damage) {
+    return (health - damage < 0) ? 0 : health - damage
+}
+
+function pipeFix(num) {
+
+    let arr = []
+    let stop = num.pop()
+
+    if (num == false) {
+        return [2]
+    } else {
+        for (let i = num[0]; i <= stop; i++) {
+            arr.push(i)
+        }
+        return arr
+    }
+}
+
+
+// pipeFix([1,7])
+
+
+
+function powersOfTwo(n) {
+    var result = [];
+    for (var i = 0; i <= n; i++) {
+        result.push(Math.pow(2, i));
+    }
+    return result;
+}
+
+
+function sumStr(a, b) {
+
+    return (Number(a) + Number(b)).toString()
+
+}
+// sumStr("4","5")
+
+function getRealFloor(n) {
+
+    if (n <= 0) {
+        return n
+    } else if (n < 13) {
+        return n - 1
+    } else if (n > 13) {
+        return n - 2
+    }
+
+}
+
+//   getRealFloor(1)
+
+function unusualFive() {
+    return ['a', 'a', 'a', 'a', 'a'].length
+}
+
+function checkForFactor(base, factor) {
+    return (base % factor === 0) ? true : false
+}
+// checkForFactor(653, 7)
+
+function setAlarm(emp, vac) {
+    return (emp == true && vac == false) ? true : false
+}
+
+
+function move(pos, roll) {
+    return pos + roll * 2
+}
+
+function solution(a, b) {
+    return (b.length > a.length) ? `${a}${b}${a}` : `${b}${a}${b}`
+}
+
+function isOpposite(s1, s2) {
+
+    if (!s1.length || s1.length !== s2.length) {
+        return false
+    } else {
+
+        let lo = s1.split('').map(a => a == a.toLowerCase())
+        let up = s2.split('').map(a => a == a.toUpperCase())
+
+        return lo.every((a, i) => a === up[i])
+
+
+    }
+}
+
+function buildString(...template) {
+    return `I like ${template.join(', ')}!`;
+}
+
+function stringClean(s) {
+    return s.replace(/\d+/g, "")
+}
+
+function testEven(n) {
+    return (n % 2 == 0) ? true : false
+}
+testEven(1)
+
+function vowelOne(str) {
+
+    return str.toLowerCase().split('').map(x =>
+        (x === 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
+            ? 1 : 0).join("")
+
+}
